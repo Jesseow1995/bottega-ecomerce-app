f03e468448c0f0f55a60602dc1c42e3abebce5f3
Jesse Williamson <jesseowilliamson@gmail.com>
2020-10-13 21:08:58 -0600

    added formField input and added email and password to signin form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -7,7 +7,7 @@ class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
-                <SignInForm />
+                <SignInForm className='sign-in__form' />
             </div>
         )
     }
