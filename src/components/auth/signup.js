b0dc02850e2a899be4c4ebda3f40de216fe5ac09
Jesse Williamson <jesseowilliamson@gmail.com>
2020-10-18 21:00:57 -0600

    developed signup form and a modified form button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import SignUpForm from './signupForm';
+import PageTitle from '../pageTitle';
 
-export default class SingUp extends Component {
+
+class SignUp extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div className="sign-up">
-                Sign Up
+            <div className='sign-up'>
+                <PageTitle className="sign-up__pageTitle" title="Register" />
+                <SignUpForm onSubmit={this.onSubmit} className='sign-up__form' />
             </div>
         )
     }
 }
+
+export default SignUp;
