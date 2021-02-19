c316cdcfd8ae616cb24ef362f4add09f02379bbd
Jesse Williamson <jesseowilliamson@gmail.com>
2021-02-18 19:15:31 -0700

    set up shipping and payment forms

--- src/components/information/payment.js
+++ src/components/information/payment.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import SignInForm from './signinForm';
 import PageTitle from '../pageTitle';
+
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
-class SignIn extends Component {
+import PaymentForm from './paymentForm';
+
+class Payment extends Component {
 
     componentDidMount() {
         this.props.setHeaderLinks([]);
@@ -14,20 +16,19 @@ class SignIn extends Component {
     }
 
     onSubmit = (fields) => {
-        this.props.signIn(fields);
-        this.props.history.push('/account');
+        console.log(fields);
     }
 
     render() {
         return (
             <div className='sign-in'>
-                <PageTitle className="sign-in__page-title" title="Login" />
-                <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
+                <PageTitle className='sign-in__page-title' title='Payment Information' />
+                <PaymentForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
     }
 }
 
-SignIn = connect(null, actions)(SignIn);
+Payment = connect(null, actions)(Payment);
 
-export default SignIn;
+export default Payment;
